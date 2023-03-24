c1e7f3ebd43bfa58b7ef6a2a945a8028732c1c8a
lemuzhi <1163648924@qq.com>
2023-03-24 10:47:05 +0800

    完成

---
+++ go.mod
@@ -0,0 +1,5 @@
+module open-api-example
+
+go 1.20
+
+require github.com/lemuzhi/open-api v0.0.0-20230323090328-c2fc3dd001b8 // indirect
---
+++ main.go
@@ -0,0 +1,6 @@
+package main
+
+func main() {
+	//tk := tiktok_open_api.NewTiktokOpenApi("aaaa", "bbbb", "cccc")
+	//tk.Jscode2session()
+}
